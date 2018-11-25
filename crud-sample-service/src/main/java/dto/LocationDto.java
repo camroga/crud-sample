6d990575176defb07242d8a08752ea0fdc5ca8ec
Camilo Andrés Rodríguez Garzón <cam_rod@hotmail.com>
2018-11-26 06:58:55 +1000

    Update LocationDto.java

--- crud-sample-service/src/main/java/dto/LocationDto.java
+++ crud-sample-service/src/main/java/dto/LocationDto.java
@@ -1,1 +1,43 @@
-package dto;public class LocationDto {    private String streetAddress;    private String city;    private String postCode;    public String getStreetAddress() {        return streetAddress;    }    public void setStreetAddress(String streetAddress) {        this.streetAddress = streetAddress;    }    public String getCity() {        return city;    }    public void setCity(String city) {        this.city = city;    }    public String getPostCode() {        return postCode;    }    public void setPostCode(String postCode) {        this.postCode = postCode;    }    @Override    public String toString() {        return "LocationDto{" +                "streetAddress='" + streetAddress + '\'' +                ", city='" + city + '\'' +                ", postCode='" + postCode + '\'' +                '}';    }}
+package dto;
+
+public class LocationDto {
+  
+    private String streetAddress;
+  
+    private String city;
+  
+    private String postCode;
+  
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+  
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+  
+    public String getCity() {
+        return city;
+    }
+  
+    public void setCity(String city) {
+        this.city = city;
+    }
+  
+    public String getPostCode() {
+        return postCode;
+    }
+  
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+  
+    @Override
+    public String toString() {
+        return "LocationDto{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", postCode='" + postCode + '\'' +
+                '}';
+    }
+}
